b1b598b6c806261030e09e13f451d4dad407c2ae
neo2wicked <capricorn@Live.com>
2024-07-24 20:29:01 -0500

    day 2, project almost finished

---
+++ scripts/main.js
@@ -0,0 +1,4 @@
+// This is where you'd put your JavaScript, if you weren't a complete waste of bandwidth
+console.log("Welcome to my digital cry for attention!");
+
+// Add more "functionality" here, I dare you
